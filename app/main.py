542e3946dc18682bcee2a443684e6ae439d6df40
khw3991 <khw430613@gmail.com>
2026-02-05 04:11:41 +0900

    week3
    Refactored main.py to include routers for items and students, and removed hardcoded item data and related endpoints.

--- app/main.py
+++ app/main.py
@@ -1,41 +1,11 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException, APIRouter
+from app.items_router import items_router
+from app.students_router import Students_router
 
 app = FastAPI()
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-items = [
-    {'id':1, 'data':'apple', 'rating':3},
-    {'id':2, 'data':'banana', 'rating': 4},
-    {'id':3, 'data':'cherry', 'rating' : 5}
-]
-
-class ItemCreate(BaseModel):
-    data: str
-    rating: float
-
-
-@app.post('/items')
-def create_item(payload: ItemCreate):
-    item = {'id': len(items)+1, **payload.model_dump()}
-    items.append(item)
-    return item
-
-@app.get('/items')
-def get_items(q :float | None=None):
-    if q == None :
-        return {"data": items}
-    result=[]
-    for item in items :
-        if item.get('rating')>=q :
-            result.append(item)
-    if len(result)==0 :
-        raise HTTPException(status_code=404, detail='Item not found')
-    return result
-
-@app.get('/items/{item_id}')
-def get_item_by_id(item_id: int):
-    for item in items:
-        if item.get('id') == item_id:
-            return item
-    raise HTTPException(status_code=404, detail='Item not found')
-
+app.include_router(router=items_router)
+app.include_router(router=Students_router)
